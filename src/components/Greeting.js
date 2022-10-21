e78f778f04da8b26ff003b24e0472969b2d309cf
Theophileaseh <theophilenwachan@gmail.com>
2022-10-21 15:48:32 +0100

    Fix linter errors

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { getMessages } from '../redux/messages';
+import './Greeting.css';
 
 const Greeting = () => {
   const messages = useSelector((state) => state.messages);
